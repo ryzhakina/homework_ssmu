b5c13b623ce110170268bc341ca431a17191c3d2
n.kusurova <natasha@kusurov.ru>
2023-04-02 13:14:28 +0300

    Добавлено 4 ДЗ

---
+++ homework4/Counter.cs
@@ -0,0 +1,17 @@
+﻿namespace homework4;
+class Counter
+{
+    public delegate void Container(int num);
+    public event Container OnCount;
+    
+    public void Count()
+    {
+        for (int i = 0; i <= 100; i++)
+        {
+            if (i == 69)
+            {
+                OnCount(i);
+            }
+        }
+    }
+}
